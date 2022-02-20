efe6a1bd99bdc97820488092610a7b4e3f9f986f
verazo <verazo@bancolombia.com.co>
2022-02-19 20:53:39 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './components/head/head.component';
+import { BodyComponent } from './components/body/body.component';
+import { FootComponent } from './components/foot/foot.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    BodyComponent,
+    FootComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
